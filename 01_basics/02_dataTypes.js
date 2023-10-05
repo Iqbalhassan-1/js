eed11005e71ac79dffa4bfda3ef82adc02f52ca7
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2023-10-05 11:01:13 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"
+// console.log(3+3)
+
+// 1 => Number
+// 2 => bigint
+// 3 => string => ""
+// 4 => boolean => true/false 
+// 5 => null => standalone value // type of null is object .
+// 6 => undefined => variable declare and !assign value to variable . e.g let a;
+// 7 => symbol => use for unique .
+// 8 => Object 
