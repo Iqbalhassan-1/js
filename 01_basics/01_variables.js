df4e4a6adb2656f6f43d3d508f44b0d98ad72e1e
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2023-10-05 10:21:22 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=15554;
+let myName="iqbal";
+city ="peshawar";
+myName="Hassan";
+city="swat"
+// here we can't reassign the value to const keyword that give me error;
+// accountId=21112;
+// when let keyword assign value then reassign value to let keyword possible;
+console.table([accountId,myName,city])
