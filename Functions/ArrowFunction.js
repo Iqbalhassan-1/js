29b15a82274a6497abedc89905acfa7a952448f6
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2023-10-27 18:37:26 +0000

    arro

--- Functions/ArrowFunction.js
+++ Functions/ArrowFunction.js
@@ -1,6 +1,12 @@
 const user ={
     userName:"iqbal",
     price:88,
-    welcomeMessage:
-
+    welcomeMessage:function(){
+        console.log(`${this.userName} Welcome to Js the price is ${this.price}`)
+    }
 }
+// this keyword refer to current context
+user.welcomeMessage() // show me userName is iqbal but now when change the value of userName is Hassan then what can be show ?
+user.userName="Hassan";
+// now call the function inside object what can be show let's check the result 
+user.welcomeMessage() // So now they show me userName is Hassan why ? bcz i changed the object of value when change the object value then change and inside the function i use the "This" keyword they refer to current context so that why they show me Hassan.
