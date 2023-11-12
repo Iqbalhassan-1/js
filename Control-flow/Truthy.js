7014520940b50fe4f439ac5292e3b6c697c9cb3a
Iqbal Hassan <107492954+Iqbalhassan-1@users.noreply.github.com>
2023-11-12 20:30:16 +0000

    ADDed ?? and ? : ""

--- Control-flow/Truthy.js
+++ Control-flow/Truthy.js
@@ -22,3 +22,17 @@ if(userEmail){
 // 4:[] => true empty array 
 // 5:{} => true empty object
 // 6:function(){} => truthy empty function 
+
+// Nullish Coalescing Operator (??) => working on null undefined
+ 
+// let val1;
+// val1=5??10 
+// val1=null ?? 10 // means agr value null hai to 10 assign krlu
+// val1=undefined??15 // same above
+// val1=null??10??20
+// console.log(val1)
+
+// Terniary Operator
+// condition ?true:false
+const iceTeaPrice=100
+iceTeaPrice <= 80 ? console.log("true"): console.log("false");;
