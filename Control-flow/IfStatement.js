2b5efc1c2ed68788687173d9091394a4203bba32
Iqbal Hassan <107492954+Iqbalhassan-1@users.noreply.github.com>
2023-11-12 19:21:45 +0000

    if and esle statement done

---
+++ Control-flow/IfStatement.js
@@ -0,0 +1,42 @@
+'use strict'
+// IF
+// const isUserLoggin=true;
+// if(isUserLoggin){
+//     console.log("User loggin");
+// }
+
+// const temprature=41;
+// if(temprature<50){
+//     console.log("less than 50");
+// }else{
+//     console.log("not less than 50");
+// }
+
+// const score=200;
+// if(score>100){
+//     const power="Fly" // scope
+//      console.log(`user power:${power}`);
+// } 
+// console.log(`user power:${power}`); // Not accessible outside body power
+
+
+
+// < => less than
+// > => greater than
+// <= => less than or equal to
+// >= => greater than or equal to
+// 2=="2" => equal to
+// != => equal Not 
+// 2==="2" =>  equal and also check type " strickly"
+
+
+// const balance=1000
+// if(balance<500){
+//     console.log("less than 500");
+// }else if (balance<750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("greater than 1000");
+// }
