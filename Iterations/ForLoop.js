77940ebd1ef061f27e0d85b828bad7d890535d0b
Iqbal Hassan <107492954+Iqbalhassan-1@users.noreply.github.com>
2023-11-13 06:39:50 +0000

    for loop added with break and continue

--- Iterations/ForLoop.js
+++ Iterations/ForLoop.js
@@ -19,4 +19,32 @@
     }
  }
 
+ let myArray=['SpiderMan','Batman','SuperMan']
+ for (let index = 0; index < myArray.length; index++) {
+   const element = myArray[index];
+   // console.log(element);
+ }
+
+ // break and continue
+
+ for (let index = 1; index <= 20; index++) {
+   if(index===5){
+      console.log(`Detected 5`);
+      break // loop End
+   }
+  console.log(`value of i is ${index}`);
+ }
+
+ // continue
+
+ for (let index = 1; index <= 20; index++) {
+   if(index===5){
+      console.log(`Detected 5`);
+      continue // loop continue
+   }
+  console.log(`value of i is ${index}`);
+ }
+
+
+
  
