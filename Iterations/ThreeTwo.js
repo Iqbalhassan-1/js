a09a9b1053dfcba244ed824a1a5985c19c8f69d5
Iqbal Hassan <107492954+Iqbalhassan-1@users.noreply.github.com>
2023-11-15 06:10:02 +0000

    for in loop and for of loop

--- Iterations/ThreeTwo.js
+++ Iterations/ThreeTwo.js
@@ -5,6 +5,30 @@ const myObject={
    swift:"swift by apple"
 }
 
-for (const key in object) {
-   
+for (const key in myObject) {
+   // console.log(key); // for key
 }
+for (const key in myObject) {
+   // console.log(`${key} shortcut for ${myObject[key]}`);
+}
+
+// now for array
+
+const arr=["arr","arr2","arr3","arr4"]
+// in key they will show me index 0,1,2,3,4
+for (const key in arr) {
+//  console.log(arr[key]);
+}
+
+// now for map
+const map=new Map()
+map.set('Pak',"Pakistan")
+map.set('Afg',"Afghanistan")
+map.set('Fra',"France")
+map.set('Pak',"Pakistan")
+
+for (const key in map) {
+//  console.log(key);
+}
+// do not show me output bcz map is not iterable 
+
