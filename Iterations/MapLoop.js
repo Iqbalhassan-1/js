a0a5c6eefcd13ea7c4cb624c639c0e9b04845c12
Iqbal Hassan <107492954+Iqbalhassan-1@users.noreply.github.com>
2023-11-15 09:20:38 +0000

    map - loop and chaining with filter

--- Iterations/MapLoop.js
+++ Iterations/MapLoop.js
@@ -1,2 +1,34 @@
+
+// MAP => Return New Array 
 const myNumbers=[1,2,3,4,5,6,7,8,9,10]
- 
+//  const newNums=myNumbers.map((num)=>num+5)
+
+//  console.log(newNums);
+ // now solve on foreach
+
+//  const newNumsForEach=[]
+//  myNumbers.forEach((num)=>{
+//     if(num>0){
+//         newNumsForEach.push(num+5)
+//     }
+//  })
+//  console.log(newNumsForEach);
+
+//*************************continue *****/
+
+
+// Chaining E.G map()=>map()=>filter()
+
+// const newNums=myNumbers
+//               .map((num1)=>num1*10)
+//               .map((num2)=>num2+1)
+// console.log(newNums);
+// // OutPut => num1 => 1*10 then num2=>10+1 =11 chaining exmple
+
+const newNums=myNumbers
+              .map((num1)=>num1*10)
+              .map((num2)=>num2+1)
+              .filter((num3)=>num3>=40)
+console.log(newNums);
+// OutPut => num1 [] then num2 [] after filter then give value true 
+
