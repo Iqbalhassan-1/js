1e4147296f916c5f3f813a809fd40380d4382d4b
iqbal hassan <emiqball111@gmail.com>
2023-11-27 00:46:21 +0500

    Async theory added

---
+++ Async/AsyncTheory.js
@@ -0,0 +1,10 @@
+// Default Javascript => Synchronous => line by line
+//                       Single threaded
+// Execuation Context =>  execute one line of code at a time
+// // Each operation wait for the last to complete before executing
+
+// Blocking Coding vs Non Blocking Code
+// Blocking Code => block the flow of programe
+// Non Blocking Code => Does Not block execution
+
+// fetch()=> promises => high priority
